360f3d843bb4c7ca75926899c6524b07680906c4
Benji <benjaminpeto23@gmail.com>
2022-05-19 11:11:13 +0200

    installed nanoid npm package, deleted App.css, wrote handleChange and handleSubmit function, added basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
-import './App.css';
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+type TodoType = {
+  id: string,
+  name: string,
+}
 
 function App() {
+  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState<TodoType[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>)=> {
+    const value = e.target.value;
+    setNewTodo(value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        id: nanoid(),
+        name: newTodo,
+      },
+    ]);
+    setNewTodo("");
+  };
+
   return (
     <div className="App">
-      Hello
+      <h1>Todo App</h1>
+      <input
+        type='text'
+        placeholder='Write your task here..'
+        value={newTodo}
+        onChange={handleChange}
+      />
+      <button
+        type='submit'
+        onClick={handleSubmit}
+      >
+        Add
+      </button>
+      <ul>
+        {
+          todos.map(todos => (
+            <li key={todos.id}>{todos.name}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
